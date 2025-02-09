90e0c4e7695f782ebabd3fd0b489cf5dc03d774e
Vivaan Lunkad <lunkad.v@northeastern.edu>
2025-02-08 23:22:16 -0500

    WEREBACK

---
+++ hackbeanpot/lib/secrets.dart
@@ -0,0 +1,4 @@
+class Secrets {
+  static const String mapboxPublicToken =
+      'pk.eyJ1IjoibHVua2FkdiIsImEiOiJjbTZ3ejhkazYwZnhlMm5vZ2JhNWVrdzNmIn0.pgMEb-eN8m7pLYf-FOBEVAf'; // Replace with your actual public token
+}
