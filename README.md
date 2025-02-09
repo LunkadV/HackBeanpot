71fb14417b849243943555a7fc3249f72a5fd251
rimjhimsingh2107 <123257628+rimjhimsingh2107@users.noreply.github.com>
2025-02-09 09:41:53 -0500

    roadtrip-remix

--- README.md
+++
@@ -1,1 +0,0 @@
-# HackBeanpot
--- hackbeanpot/lib/secrets.dart
+++
@@ -1,4 +0,0 @@
-class Secrets {
-  static const String mapboxPublicToken =
-      'pk.eyJ1IjoibHVua2FkdiIsImEiOiJjbTZ3ejhkazYwZnhlMm5vZ2JhNWVrdzNmIn0.pgMEb-eN8m7pLYf-FOBEVAf'; // Replace with your actual public token
-}
